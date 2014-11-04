d80caa3c1585c6ac8e13176004c8aa40885ae3c2
Vincent Lee <lee.m.vincent@gmail.com>
2014-11-04 14:24:23 -0800

    Adding Reminder NSObject

---
+++ MapApp/Reminder.swift
@@ -0,0 +1,19 @@
+//
+//  Reminder.swift
+//  MapApp
+//
+//  Created by Vincent Lee on 11/4/14.
+//  Copyright (c) 2014 VincentLee. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class Reminder: NSManagedObject {
+
+    @NSManaged var name: String
+    @NSManaged var date: String
+    @NSManaged var radius: NSNumber
+    @NSManaged var coordinate: String
+
+}
